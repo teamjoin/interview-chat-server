e9bc003ee608c2bf01422c7d29ca667eab9d205a
Loïc Renaud <loic1.renaud@hotmail.fr>
2022-06-21 10:22:07 +0200

    Add docker and nginx

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ var rf = require('random-facts');
 
 const io = require('socket.io')(server, { cors: { origin: '*' } });
 
-const users = [{ username: 'Edouard', color: 'blue' }, { username: 'Dimitri', color: 'red'}, { username: 'Thibaut', color: 'pink'}];
+const users = [{ username: 'Edouard', color: '#99ccff' }, { username: 'Dimitri', color: '#99ffcc'}, { username: 'Thibaut', color: '#cc99ff'}];
 
 const generateUser = () => {
     return users[Math.floor(Math.random() * users.length)]
@@ -15,24 +15,19 @@ const generateUser = () => {
 // events will go here...
 io.on('connection', (socket) => {
     console.log('New User connected', socket.id);
-
     socket.on('send-message', (data) => {
-        // console.log(`Message from client: ${data.text}, whoose id is: ${data.from}`);
         console.log('data',data);
         io.emit('new-message', data);
     });
-
-
     socket.on('forceDisconnect', function(){
         socket.disconnect(true);
-    })
-
+    });
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
 });
 setInterval(() => {
-    io.emit('new-message', { text: rf.randomFact(), user: generateUser(), date: Date.now() } );
+    io.emit('new-message', { type:'text', text: rf.randomFact(), user: generateUser(), date: Date.now() } );
 }, 4000)
 
 const PORT = 3030; // process.env.PORT || 80;
