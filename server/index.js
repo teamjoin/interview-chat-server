eb1c531210fc9fd41cd6f7311abdf1458333fb8d
Loïc Renaud <loic1.renaud@hotmail.fr>
2022-06-21 11:40:15 +0200

    Update

--- server/index.js
+++ server/index.js
@@ -3,10 +3,18 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-var rf = require('random-facts');
+const rf = require('random-facts');
+const cors = require('cors');
+const { Server } = require('socket.io')
+const io = new Server(server, { cors: { origin: '*' } });
 
-const io = require('socket.io')(server, { cors: { origin: '*' } });
-
+const corsOptions = {
+    origin: function(origin, callback) {
+        callback(null, true);
+    },
+    credentials: true
+};
+app.use(cors(corsOptions));
 const users = [{ username: 'Edouard', color: '#99ccff' }, { username: 'Dimitri', color: '#99ffcc'}, { username: 'Thibaut', color: '#cc99ff'}];
 
 const generateUser = () => {
